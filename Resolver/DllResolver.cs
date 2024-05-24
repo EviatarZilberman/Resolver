dd09f605347c41b7eefd9608ec94327a64471406
Eviatar Zilberman <esztbk@gmail.com>
2024-05-24 17:05:27 +0300

    Add project files.

---
+++ Resolver/DllResolver.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Reflection;
+
+namespace Resolver
+{
+    public class DllResolver
+    {
+        private object? ClassInstance { get; set; } = null;
+        private Type? StaticClass { get; set; } = null;
+        private string DllPath { get; set; }
+
+        public DllResolver(string dllPath)
+        {
+            this.DllPath = dllPath;
+        }
+
+        public void UseStaticMethod(string methodName, object[]? arguments = null) // For void static methods.
+        {
+            if (StaticClass == null) throw new InvalidOperationException("Static class not loaded.");
+
+            MethodInfo? method;
+            if (arguments == null)
+            {
+                method = StaticClass.GetMethod(methodName, Type.EmptyTypes);
+            }
+            else
+            {
+                Type[] argumentTypes = arguments.Select(arg => arg.GetType()).ToArray();
+                method = StaticClass.GetMethod(methodName, argumentTypes);
+            }
+
+            if (method == null) throw new InvalidOperationException($"Method '{methodName}' not found in static class.");
+
+            method.Invoke(null, arguments);
+        }
+
+        public object UseStaticMethodReturn(string methodName, object[]? arguments = null) // For return static methods.
+        {
+            if (StaticClass == null) throw new InvalidOperationException("Static class not loaded.");
+
+            MethodInfo? method;
+            if (arguments == null)
+            {
+                method = StaticClass.GetMethod(methodName, Type.EmptyTypes);
+            }
+            else
+            {
+                Type[] argumentTypes = arguments.Select(arg => arg.GetType()).ToArray();
+                method = StaticClass.GetMethod(methodName, argumentTypes);
+            }
+
+            if (method == null) throw new InvalidOperationException($"Method '{methodName}' not found in static class.");
+
+            return method.Invoke(null, arguments);
+        }
+
+        public void UseMethods(string methodName, object[]? arguments = null) // For internal void methods.
+        {
+            if (ClassInstance == null) throw new InvalidOperationException("Class instance not loaded.");
+            MethodInfo? method = ClassInstance.GetType().GetMethod(methodName);
+            if (method == null) throw new InvalidOperationException($"Method '{methodName}' not found in instance class.");
+            method.Invoke(ClassInstance, arguments);
+        }
+
+        public object UseMethodsReturn(string methodName, object[]? arguments = null) // Internal methods with return.
+        {
+            if (ClassInstance == null) throw new InvalidOperationException("Class instance not loaded.");
+            MethodInfo? method = ClassInstance.GetType().GetMethod(methodName);
+            if (method == null) throw new InvalidOperationException($"Method '{methodName}' not found in instance class.");
+            return method.Invoke(ClassInstance, arguments);
+        }
+
+        public void LoadInstanceAble(string className, object[]? args = null) // Loads the resolver for Instance of a class.
+        {
+            if (string.IsNullOrWhiteSpace(className))
+            {
+                throw new ArgumentNullException(nameof(className));
+            }
+
+            try
+            {
+                // Load the assembly from the specified DLL path
+                Assembly? assembly = Assembly.LoadFrom(DllPath);
+
+                // Get the type of the class from the assembly
+                Type? type = assembly.GetType(className);
+
+                if (type == null)
+                {
+                    throw new TypeLoadException($"Type '{className}' not found in assembly '{DllPath}'.");
+                }
+
+                // Create an instance of the class
+                this.ClassInstance = Activator.CreateInstance(type, args);
+                this.StaticClass = type;
+            }
+            catch (Exception ex)
+            {
+                // Handle or log the exception as needed
+                throw new InvalidOperationException("Failed to load and create instance of the specified type.", ex);
+            }
+        }
+
+        public void LoadStatic(string className) // Loads static classes.
+        {
+            if (string.IsNullOrWhiteSpace(className))
+            {
+                throw new ArgumentNullException(nameof(className));
+            }
+
+            try
+            {
+                // Load the assembly from the specified DLL path
+                Assembly? assembly = Assembly.LoadFrom(DllPath);
+
+                // Get the type of the class from the assembly
+                Type? type = assembly.GetType(className);
+                if (type == null)
+                {
+                    throw new TypeLoadException($"Type '{className}' not found in assembly '{DllPath}'.");
+                }
+
+                if (type.IsAbstract)
+                {
+                    this.StaticClass = type;
+                }
+                else
+                {
+                    throw new InvalidOperationException("Type is not static.");
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle or log the exception as needed
+                throw new InvalidOperationException("Failed to load and create instance of the specified type.", ex);
+            }
+        }
+    }
+}
---
+++ Resolver/Program.cs
@@ -0,0 +1,39 @@
+﻿using Resolver;
+
+/*class Program
+{
+    static void Main(string[] args)
+    {*/
+        string dllPath = "C:\\Users\\User\\Projects\\Python\\ThePackage\\ThePackage\\bin\\Debug\\net8.0\\ThePackage.dll";
+        DllResolver resolverIns = new DllResolver(dllPath);
+        DllResolver resolverSt = new DllResolver(dllPath);
+
+        // Load instance class
+        string className = "ThePackage.NewMathClass";
+        resolverIns.LoadInstanceAble(className);
+
+
+        Console.WriteLine("Static:");
+        Console.WriteLine();
+        // Load static class
+        string staticClassName = "ThePackage.MathClass";
+        resolverSt.LoadStatic(staticClassName);
+        Console.WriteLine(resolverSt.UseStaticMethodReturn("Plus", new object[] { 2,3 }));
+        resolverSt.UseStaticMethod("SayHi");
+
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine();
+
+        Console.WriteLine("Instance:");
+        Console.WriteLine();
+        // Use instance method
+        resolverIns.UseMethods("SayBye");
+        Console.WriteLine(resolverIns.UseMethodsReturn("Plus", new object[] { 3, 7 }));
+        resolverIns.UseStaticMethod("SayHi");
+        Console.WriteLine(resolverIns.UseStaticMethodReturn("SayHi", new string[] { "Eviatar" }));
+
+        Console.ReadLine();
+/*    }
+}*/
